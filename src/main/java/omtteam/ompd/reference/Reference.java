d3aa394cb31364878c1c6ce668a1814880426830
Keridos <2742845+Keridos@users.noreply.github.com>
2020-03-20 04:25:21 +0100

    fix curseforge change-logs for artifacts + mcmod.info stuff + gradle refactoring

--- src/main/java/omtteam/ompd/reference/Reference.java
+++ src/main/java/omtteam/ompd/reference/Reference.java
@@ -4,6 +4,5 @@ public class Reference {
     public static final String MOD_ID = "ompd";
     public static final String NAME = "Open Modular Passive Defence";
     public static final String VERSION = "@VERSION@";
-    public static final String OMLIB_MIN_VERSION = "3.0.0-91";
-    public static final String DEPENDENCIES = "required-after:omlib@" + OMLIB_MIN_VERSION;
+    public static final String DEPENDENCIES = "required-after:omlib@[0.0.0,)";
 }
